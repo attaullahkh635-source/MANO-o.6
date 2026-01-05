66ee6797791b3e4171d838fad67df9568ae6c5f5
attaullahkh635-source <attaullahkh635@gmail.com>
2026-01-06 01:47:10 +0500

    Update owner.js

--- rdx/commands/owner.js
+++ rdx/commands/owner.js
@@ -17,7 +17,7 @@ module.exports = {
     const { threadID, messageID } = event;
 
     const ownerPics = [
-      'https://i.ibb.co/Cp13xBsR/b2edd4c03615.jpg',
+      'https://i.postimg.cc/L5sgqq0w/FB-IMG-17605572622668081.jpg',
       '',
       '',
       ''
@@ -30,21 +30,21 @@ module.exports = {
 ║   ✨ 𝐁𝐎𝐓 𝐎𝐖𝐍𝐄𝐑 𝐈𝐍𝐅𝐎 ✨   ║
 ╠═══════════════════════════╣
 ║                           ║
-║  👤 𝐍𝐚𝐦𝐞: SARDAR RDX    ║
+║  👤 𝐍𝐚𝐦𝐞: ATTAULLAH    ║
 ║                           ║
 ╠═══════════════════════════╣
 ║  📱 𝐂𝐨𝐧𝐭𝐚𝐜𝐭 𝐈𝐧𝐟𝐨:          ║
 ║                           ║
 ║  🌐 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤:              ║
-║  facebook.com/SARDAR RDX ║
+║  facebook.com/KHUHARO ║
 ║                           ║
 ║  📲 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩:              ║
-║  wa.me/923301068874       ║
+║  wa.me/923259260635       ║
 ║                           ║
 ╠═══════════════════════════╣
 ║  🤖 𝐁𝐨𝐭 𝐃𝐞𝐭𝐚𝐢𝐥𝐬:           ║
 ║                           ║
-║  📛 Name: ${config.BOTNAME || 'SARDAR RDX'}
+║  📛 Name: ${config.BOTNAME || 'AYESHA'}
 ║  ⚡ Prefix: ${config.PREFIX || '.'}
 ║  💻 Version: 0.5       ║
 ║  🛠️ Framework: RDX-FCA    ║
